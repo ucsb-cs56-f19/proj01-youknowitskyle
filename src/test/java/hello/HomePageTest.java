102db2b402c7ffa48614d7ea7f23a20e904f4f52
Kyle Wong <lemondeer123@gmail.com>
2019-11-17 14:53:54 -0800

    kw - integrated and tested page 1 on navbar

--- src/test/java/hello/HomePageTest.java
+++ src/test/java/hello/HomePageTest.java
@@ -61,4 +61,11 @@ public class HomePageTest {
                 .andExpect(xpath("/html/body/div/nav/a").exists())
                 .andExpect(xpath("/html/body/div/nav/a").string("lab07"));
     }
+
+    @Test
+    public void getHomePage_hasCorrectFirstPage() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.TEXT_HTML)).andExpect(status().isOk())
+                .andExpect(xpath("//*[@id=\"navbarTogglerDemo03\"]/ul[1]/li[2]/a").exists())
+                .andExpect(xpath("//*[@id=\"navbarTogglerDemo03\"]/ul[1]/li[2]/a").string("Earthquakes"));
+    }
 }
